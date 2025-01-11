5b5d8126327c4769a931ef602ac9459abd450e5a
Gustavo Lima <gustavolimamendes2@gmail.com>
2025-01-11 00:00:38 -0300

    feat: estilizando o cabeçalho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./componentes/container/container.component";
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent],
+  imports: [RouterOutlet, CommonModule, ContainerComponent, CabecalhoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
